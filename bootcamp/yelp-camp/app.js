3f0037e8d9cf92c90d0b3d70b7ae2bb5886b50fa
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2019-12-09 09:54:00 +0530

    Data persistance added

--- bootcamp/yelp-camp/app.js
+++ bootcamp/yelp-camp/app.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-let campGrounds = [
-  {
-    name: 'Lonawala',
-    image: 'https://images.unsplash.com/photo-1521206644285-8db1549e484f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80'
-  },
-  {
-    name: 'Lonawala',
-    image: 'https://images.unsplash.com/photo-1521206644285-8db1549e484f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80'
-  },
-  {
-    name: 'Lonawala',
-    image: 'https://images.unsplash.com/photo-1521206644285-8db1549e484f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80'
-  },
-  {
-    name: 'Lonawala',
-    image: 'https://images.unsplash.com/photo-1521206644285-8db1549e484f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80'
-  }
-];
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/campgrounds', { useNewUrlParser: true, useUnifiedTopology: true });
+const campGroundsSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+const CampGrounds = mongoose.model('CampGrounds', campGroundsSchema);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.render('landing.ejs');
 });
-app.get('/campgrounds', (req, res) => {
+app.get('/campgrounds', async (req, res) => {
+  const campGrounds = await CampGrounds.find();
   res.render('campgrounds.ejs', { campGrounds });
 });
-app.post('/campgrounds', (req, res) => {
-  campGrounds.push(req.body);
-  res.redirect('/campgrounds');
+app.post('/campgrounds', async (req, res) => {
+  try {
+    const campGrounds = new CampGrounds(req.body);
+    await campGrounds.save();
+    res.redirect('/campgrounds/new');
+  } catch (err) {
+    console.log(err);
+    res.redirect('/campgrounds/new');
+  }
 });
-app.get('/campgrounds/new', (req, res) => {
+app.get('/campgrounds/new', async (req, res) => {
   res.render('new.ejs');
 });
 app.listen(4000, () => console.log(`Express server is running at http://localhost:4000`));
