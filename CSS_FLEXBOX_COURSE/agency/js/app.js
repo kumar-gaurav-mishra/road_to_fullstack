076c4ada45c29f8a042b2bd025e1a7e08c4fc0c7
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2019-12-18 07:49:39 +0530

    Adeed header and nav bar

--- CSS_FLEXBOX_COURSE/agency/js/app.js
+++ CSS_FLEXBOX_COURSE/agency/js/app.js
@@ -1,3 +1,23 @@
-$(document).ready(function () {
-    $('.preloader').fadeOut();
-})
+$(document).ready(function() {
+  //preloader
+  $('.preloader').fadeOut();
+  // navBtn
+  $('.navBtn').click(function() {
+    $('.nav').toggleClass('nav__show');
+  });
+  //Video Switch
+  $('.video__switch-container').click(function() {
+    const value = $('.video__switch-btn').hasClass('btnSlide');
+    if (value) {
+      $('.video__switch-btn').removeClass('btnSlide');
+      $('#video')
+        .get(0)
+        .play();
+    } else {
+      $('.video__switch-btn').addClass('btnSlide');
+      $('#video')
+        .get(0)
+        .pause();
+    }
+  });
+});
