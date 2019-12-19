45c858ba9a3ea9a7a48b5116e6f530482e2efd4a
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2019-12-19 14:19:21 +0530

    agency project done

--- CSS_FLEXBOX_COURSE/agency/js/app.js
+++ CSS_FLEXBOX_COURSE/agency/js/app.js
@@ -31,11 +31,74 @@ $(document).ready(function() {
     }
     //other options
   });
+  // accordion
+  $('.accordion__btn').click(function() {
+    $('.accordion__btn')
+      .not(this)
+      .removeClass('rotate');
+    $('.accordion__btn')
+      .not(this)
+      .parent()
+      .next()
+      .slideUp();
+    $(this).toggleClass('rotate');
+    $(this)
+      .parent()
+      .next()
+      .slideToggle();
+  });
+  // team members
+  // iif
+  (() => {
+    const data = [
+      {
+        id: 0,
+        name: 'john doe',
+        job: 'developer',
+        text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+        favorites: ['eat', 'drink', 'sleep'],
+        img: 'img/team-1.jpg'
+      },
+      {
+        id: 1,
+        name: 'tom orange',
+        job: 'designer',
+        text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+        favorites: ['paint', 'draw', 'run'],
+        img: 'img/team-2.jpg'
+      },
+      {
+        id: 2,
+        name: 'albert cupid',
+        job: 'accountant',
+        text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+        favorites: ['math', 'physics', 'more math'],
+        img: 'img/team-3.jpg'
+      },
+      {
+        id: 3,
+        name: 'dog hugo',
+        job: 'intern',
+        text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+        favorites: ['bark', 'run', 'bite'],
+        img: 'img/team-4.jpg'
+      }
+    ];
+    $('.team-img__item').click(function() {
+      $('.team-img__item')
+        .not(this)
+        .removeClass('active');
+      $(this).addClass('active');
+      //change/add data
+      let id = $(this).attr('data-id');
+      let userData = data[id];
+      $('.team-info__member-name').text(userData.name);
+      $('.team-info__member-job').text(userData.job);
+      $('.team-info__member-text').text(userData.text);
+      $('.team-info__img').attr('src', userData.img);
+      $('.team-info__member-single-favorite-text').each(function(index) {
+        $(this).text(userData.favorites[index]);
+      });
+    });
+  })();
 });
-// accordion
-$('.accordion__btn').click(function () {
-  $('.accordion__btn').not(this).removeClass('rotate');
-  $('.accordion__btn').not(this).parent().next().slideUp();
-  $(this).toggleClass('rotate');
-  $(this).parent().next().slideToggle();
-})
