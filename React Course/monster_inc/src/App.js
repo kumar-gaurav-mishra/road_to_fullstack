9b17dcb63108ff9b418850c73fd4843676bb9ddf
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-01 16:58:59 +0530

    Some changes

--- React Course/monster_inc/src/App.js
+++ React Course/monster_inc/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> {2 + 2} and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <CardList name="kumar"/>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
---
+++ React Course/monster_inc/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const CardList = props => {
+  console.log(props);
+  return <h1>{props.name}</h1>;
+};
