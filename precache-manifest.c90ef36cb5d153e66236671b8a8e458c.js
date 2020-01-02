5c1190408df7666c3a38fa90e9ff6477c08d9c54
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-02 09:55:00 +0530

    Updates

---
+++ precache-manifest.c90ef36cb5d153e66236671b8a8e458c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df64a1b3189273b65ccea73b02b307d3",
+    "url": "/road_to_fullstack/React+Course/monster_inc/index.html"
+  },
+  {
+    "revision": "62c33832e947f8e29137",
+    "url": "/road_to_fullstack/React+Course/monster_inc/static/css/main.cdd66344.chunk.css"
+  },
+  {
+    "revision": "5aaaf92e214e1ce413f8",
+    "url": "/road_to_fullstack/React+Course/monster_inc/static/js/2.40c1e28e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/road_to_fullstack/React+Course/monster_inc/static/js/2.40c1e28e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62c33832e947f8e29137",
+    "url": "/road_to_fullstack/React+Course/monster_inc/static/js/main.6065e3c6.chunk.js"
+  },
+  {
+    "revision": "15441e9b3e7b532f2fb4",
+    "url": "/road_to_fullstack/React+Course/monster_inc/static/js/runtime-main.c1e8e638.js"
+  }
+]);
