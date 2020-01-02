8fb3dce85ce0cca3eb7e51ec6b4d7cb8759ee988
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-02 09:46:47 +0530

    monster.inc app done

--- React Course/monster_inc/src/components/card/card.component.jsx
+++ React Course/monster_inc/src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.style.css';
 export const Card = props => {
   return (
     <div className="card-container">
-      <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="" />
+      <img className="card-img" src={`https://robohash.org/${props.monster.id}`} alt="" />
       <h1>{props.monster.name}</h1>
       <p>{props.monster.email}</p>
     </div>
