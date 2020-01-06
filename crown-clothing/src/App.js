20bfc63bbf9dd442b9ba0a94951361eb06ece73c
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2020-01-06 07:16:22 +0530

    Made some progress in monsters app

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component.jsx';
+class App extends React.Component {
+  // constructor() {
+  //   super();
+  // }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
