a6b45b10d72da5bd8faedb8fab08b2d6e9f4c153
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-06 09:22:32 +0530

    broken all components and added scss

--- crown-clothing/src/components/directory/directory.component.jsx
+++ crown-clothing/src/components/directory/directory.component.jsx
@@ -11,33 +11,38 @@ class Directory extends React.Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'shop/hats',
+          bgColor: '#581f18'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: 'shop/jackets',
+          bgColor: '#f0a202'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: 'shop/sneakers',
+          bgColor: '#202c59'
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/womens'
+          linkUrl: 'shop/womens',
+          bgColor: '#d95d39'
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/mens'
+          linkUrl: 'shop/mens',
+          bgColor: '#51cb20'
         }
       ]
     };
@@ -45,8 +50,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl, bgColor }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} bgColor={bgColor} />
         ))}
       </div>
     );
--- crown-clothing/src/components/menu-item/menu-item.component.jsx
+++ crown-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, bgColor }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundColor: bgColor,
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className="background-image"></div>
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
