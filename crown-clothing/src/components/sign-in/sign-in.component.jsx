6ad7fedfbcc177666ca04c228726c80e4c7184e8
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2020-01-09 09:49:13 +0530

    some changes

--- crown-clothing/src/components/sign-in/sign-in.component.jsx
+++ crown-clothing/src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './sign-in.style.scss';
 
-class Sign extends React.Component {
+class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -10,17 +10,21 @@ class Sign extends React.Component {
       password: ''
     };
   }
-
+  handleSubmit = e => e.preventDefault();
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
   render() {
     return (
       <div className="sign-in">
         <h1>I already have an account</h1>
         <span>Sign In with your email and password.</span>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={this.state.email} placeholder="Email" required />
+          <input type="email" name="email" value={this.state.email} placeholder="Email" onChange={this.handleChange} required />
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={this.state.password} placeholder="password" required />
+          <input type="password" name="password" value={this.state.password} placeholder="password" onChange={this.handleChange} required />
           <input type="submit" value="Submit Form" />
         </form>
       </div>
--- crown-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ crown-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.style.scss';
 
-const SignInAndSignUpPage = () => <div className="sign-and-sign-up">SIGN IN</div>;
+const SignInAndSignUpPage = () => (
+  <div className="sign-and-sign-up">
+    <SignIn />
+  </div>
+);
 
 export default SignInAndSignUpPage;
