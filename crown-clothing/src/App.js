c5882e1c86fa8ad4765958fa602325d765c1289a
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-21 04:13:21 +0530

    some progress

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import firebase from 'firebase';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import auth from './firebase/firebase.utils';
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
   render() {
     return (
       <div>
