10158683707ce9a29582f56af6543f9fbceb31d8
Kumar Gaurav Mishra <kmishra5@in.ibm.com>
2020-01-20 08:13:58 +0530

    some code for firebase intigration

---
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyAEvp1jwl-GyjHkhcbNs-TzNZGPCevlwEw',
+  authDomain: 'crown-db-eaca7.firebaseapp.com',
+  databaseURL: 'https://crown-db-eaca7.firebaseio.com',
+  projectId: 'crown-db-eaca7',
+  storageBucket: 'crown-db-eaca7.appspot.com',
+  messagingSenderId: '841516581686',
+  appId: '1:841516581686:web:979e4274fd66c9de27e997'
+};
