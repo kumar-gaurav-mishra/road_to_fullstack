ae712999ee3ae324dcbf3d53a136bb41a8589e6a
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2020-01-21 04:58:24 +0530

    basic firebase setup for firebase user signin and signout is done

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import auth from './firebase/firebase.utils';
 class App extends React.Component {
   constructor() {
     super();
@@ -14,17 +13,18 @@ class App extends React.Component {
       currentUser: null
     };
   }
+  unsubscribeFromAuth = null;
   componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
-    });
+    this.unsubscribeFromAuth = firebase.auth().onAuthStateChanged(user => this.setState({ currentUser: user }));
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
   render() {
     return (
       <div>
         <Router>
-          <Header />
+          <Header currentUser={this.state.currentUser} />
           <Switch>
             <Route exact path="/">
               <HomePage />
@@ -32,7 +32,7 @@ class App extends React.Component {
             <Route exact path="/shop">
               <ShopPage />
             </Route>
-            <Route exact path="/sign-in">
+            <Route exact path="/signin">
               <SignInAndSignUp />
             </Route>
           </Switch>
