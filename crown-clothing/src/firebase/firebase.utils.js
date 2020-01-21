b1e20ff52aef856f317f3d67ce8ac61251e13531
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2020-01-21 05:38:53 +0530

    temp change not working code

--- crown-clothing/src/firebase/firebase.utils.js
+++ crown-clothing/src/firebase/firebase.utils.js
@@ -12,6 +12,10 @@ const config = {
   appId: '1:841516581686:web:979e4274fd66c9de27e997'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  
+}
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
