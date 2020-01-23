30e1a0aee682f9d6e54524d471e22e027238a997
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2020-01-23 09:54:41 +0530

    Added user action types to keep actions consistents

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import firebase from 'firebase/app';
@@ -42,8 +42,8 @@ class App extends React.Component {
             <Route exact path="/shop">
               <ShopPage />
             </Route>
-            <Route exact path="/signin">
-              <SignInAndSignUp />
+            <Route exact path="/signin" render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />)}>
+              {/* <SignInAndSignUp /> */}
             </Route>
           </Switch>
         </Router>
@@ -51,7 +51,10 @@ class App extends React.Component {
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
