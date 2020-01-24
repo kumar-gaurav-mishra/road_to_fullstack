1aa631946280d4564274d3c4ed0ef24c01c4c896
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2020-01-24 06:03:34 +0530

    Added redux to cart and able to toggle cart popup now

--- crown-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crown-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.style.scss';
 
-const CartIcon = () => (
-  <div className="cart-icon">
+const CartIcon = ({toggleCartHidden}) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
-
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+export default connect(null,mapDispatchToProps)(CartIcon);
--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import firebase from 'firebase/app';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -28,14 +28,13 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
-      <Link className="option" to="/cart">
-        <CartIcon/>
-      </Link>
+      <CartIcon />
     </div>
-    <CartDropdown/>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 export default connect(mapStateToProps)(Header);
