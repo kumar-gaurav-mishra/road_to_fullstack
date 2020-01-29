ab1a6faa41fc03ec9731f516f29aa7933dab112b
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2020-01-29 06:42:05 +0530

    Moved shop to collection overview component and simplified shop

--- crown-clothing/src/pages/shop/shop.component.jsx
+++ crown-clothing/src/pages/shop/shop.component.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './temp-shop-data';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collection: SHOP_DATA
-    };
-  }
-  render() {
-    const { collection } = this.state;
-    return (
-      <div className="shop-page">
-        {collection.map(data => (
-          <CollectionPreview key={data.id} {...data} />
-        ))}
-      </div>
-    );
-  }
-}
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 export default ShopPage;
---
+++ crown-clothing/src/redux/shop/shop.selector.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectShopCollection = createSelector([selectShop], shop => shop.collections);
