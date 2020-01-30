4122bc0906d4810a5b3e73d41dc585b048c88d9b
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2020-01-30 06:34:41 +0530

    Handled logic in the selector for shop component preview data

--- crown-clothing/src/components/collections-overview/collections-overview.component.jsx
+++ crown-clothing/src/components/collections-overview/collections-overview.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopCollection } from '../../redux/shop/shop.selector';
+import { selectShopCollectionForPreview } from '../../redux/shop/shop.selector';
 import './collections-overview.style.scss';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
-    {
-      Object.keys(collections).map(key => <CollectionPreview key={collections[key].id} {...collections[key]} />)
-    }
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollection
+  collections: selectShopCollectionForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- crown-clothing/src/redux/shop/shop.selector.js
+++ crown-clothing/src/redux/shop/shop.selector.js
@@ -3,5 +3,5 @@ import { createSelector } from 'reselect';
 const selectShop = state => state.shop;
 
 export const selectShopCollection = createSelector([selectShop], shop => shop.collections);
-
+export const selectShopCollectionForPreview = createSelector([selectShopCollection], collections => Object.keys(collections).map(key => collections[key]));
 export const selectCollection = collectionUrlParams => createSelector([selectShopCollection], collections => collections[collectionUrlParams]);
